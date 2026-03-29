5d9a4855eca16099efe84dc1c85d79fff0ea9235
Mathis Boulais <77585805+MathisBls@users.noreply.github.com>
2026-03-29 23:39:29 +0200

    Update logger references to Zephyr

--- src/lib/api/logger.ts
+++ src/lib/api/logger.ts
@@ -1,4 +1,4 @@
 import { invoke } from '$lib/invoke';
 
-export const openGaleLog = () => invoke('open_gale_log');
+export const openZephyrLog = () => invoke('open_zephyr_log');
 export const logErr = (msg: string) => invoke('log_err', { msg });
